cda6935712d16afca47f3e221e28e6dbb503e7e5
Cyryll Galon <galoncyryll@gmail.com>
2020-03-04 22:36:57 -0800

    Updates

---
+++ precache-manifest.f608f4e880571361f6eecfa150b485c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed12ba35aea2e786e2599406f7eb87f1",
+    "url": "/COVID-19-tracker/index.html"
+  },
+  {
+    "revision": "cc0ec17f9081957306cd",
+    "url": "/COVID-19-tracker/static/css/main.ac158bf2.chunk.css"
+  },
+  {
+    "revision": "a6f321fc55eaac9bf965",
+    "url": "/COVID-19-tracker/static/js/2.3426b083.chunk.js"
+  },
+  {
+    "revision": "6e7cf1f4614b5da918c98ea6a4c4ca46",
+    "url": "/COVID-19-tracker/static/js/2.3426b083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc0ec17f9081957306cd",
+    "url": "/COVID-19-tracker/static/js/main.a75ec819.chunk.js"
+  },
+  {
+    "revision": "b1864fac25e247ec416b",
+    "url": "/COVID-19-tracker/static/js/runtime-main.c542f2bb.js"
+  }
+]);
